bd4aa9a33fe3de6568f19678159ee27eb633d316
HGreender <timurkostyan@gmail.com>
2025-07-21 10:36:38 +0300

    Change main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
+import sys
+
 from src.etl.extract import Extractor
 
 
 def run_pipeline(args):
-    raw_data = None
-    extractor = Extractor(start_date=args.start_date, end_date=args.end_date)
-    raw_data = extractor.run()
-
-
+    try:
+        raw_data = None
+        if args.source == 'api':
+            extractor = Extractor(start_date=args.start_date, end_date=args.end_date)
+            raw_data = extractor.run_api_extraction()
+        elif args.source == 'json':
+            raw_data = Extractor.run_json_extraction(args.file_path)
+    except Exception as error:
+        print(f'Error: {error}')
+        sys.exit(1)
 
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
