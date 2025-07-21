4bc1c1d5abc9efd44837296a0caf651d719d3c5d
HGreender <timurkostyan@gmail.com>
2025-07-21 11:57:12 +0300

    Add validation in data setter

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -7,12 +7,14 @@ class Transformer:
     def __init__(self, raw_data: Dict[str, Any]):
         if not isinstance(raw_data, dict) or 'hourly' not in raw_data or 'daily' not in raw_data:
             raise ValueError("Transformer ValueError")
-        self.__raw_data = raw_data
+        self.__data = raw_data
 
     @property
-    def raw_data(self):
-        return self.__raw_data
+    def data(self):
+        return self.__data
 
-    @raw_data.setter
-    def raw_data(self, value: Dict[str, Any]):
-        self.__raw_data = value
+    @data.setter
+    def data(self, value: Dict[str, Any]):
+        if not isinstance(value, dict) or 'hourly' not in value or 'daily' not in value:
+            raise ValueError("Transformer ValueError")
+        self.__data = value
